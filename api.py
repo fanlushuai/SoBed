5212a9ebe71537d2aceea919b7701688220f8c6b
fanlushuai <fanlushuai@github.com>
2020-12-21 13:11:12 +0800

    fix restful

--- api.py
+++ api.py
@@ -15,55 +15,55 @@ def index():
     return RedirectResponse(url="/docs", status_code=HTTP_302_FOUND)
 
 
-@app.get("/vol_up")
+@app.get("/media/vol_up")
 def vol_up():
     pc.vol_up()
     return "ok"
 
 
-@app.get("/vol_down")
+@app.get("/media/vol_down")
 def vol_down():
     pc.vol_down()
     return "ok"
 
 
-@app.get("/music_next")
+@app.get("/media/next")
 def music_next():
     pc.music_next()
     return "ok"
 
 
-@app.get("/music_last")
+@app.get("/media/last")
 def music_last():
     pc.music_last()
     return "ok"
 
 
-@app.get("/music_play_pause")
+@app.get("/media/play_pause")
 def music_play_pause():
     pc.music_play_pause()
     return "ok"
 
 
-@app.get("/screen_off")
+@app.get("/screen/off")
 def screen_off(sec: int = 1):
     pc.screen_off(sec)
     return "ok"
 
 
-@app.get("/screen_on")
+@app.get("/screen/on")
 def screen_on():
     pc.screen_on()
     return "ok"
 
 
-@app.get("/pc_sleep")
+@app.get("/pc/sleep")
 def pc_sleep(sec: int = 5):
     pc.pc_sleep(sec)
     return "目前只写了windows，自己补充mac"
 
 
-@app.get("/pc_shutdown")
+@app.get("/pc/shutdown")
 def pc_shutdown(sec: int = 5):
     pc.pc_shutdown(sec)
     return "目前只写了windows，自己补充mac"
