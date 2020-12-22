5e440e025bc3c8af63fd7f79fd9229ed74e064c9
fanlushuai <fanlushuai@github.com>
2020-12-22 12:24:45 +0800

    fix 自动使用本地局域网ip

--- api.py
+++ api.py
@@ -1,3 +1,5 @@
+import socket
+
 import uvicorn
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
@@ -70,4 +72,9 @@ def pc_shutdown(sec: int = 5):
 
 
 if __name__ == '__main__':
-    uvicorn.run('api:app', host='0.0.0.0', port=80, log_level="info")
+    hostname = socket.gethostname()
+    local_area_network_ip = socket.gethostbyname(hostname)
+    # 若只想本机使用的服务，不允许同局域网的设备访问，使用：127.0.0.1或localhost （前提是已经做了host映射）；
+    # 若想本局域网的主机都可访问但外网不可访问，监听本主机的IP地址，例如：192.168.1.2；
+    # 若想本局域网的主机和外网都可访问，监听0.0.0.0就可以
+    uvicorn.run('api:app', host=local_area_network_ip, port=80, log_level="info")
